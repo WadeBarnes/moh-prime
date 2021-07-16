45f67769575db30b00328ddcd0a9183fdb87a26a
James <jameshollinger.ee@gmail.com>
2021-07-16 13:47:03 -0700

    placeholder

---
+++ prime-dotnet-webapi/Configuration/Unseeded Tables/SiteConfiguration.cs
@@ -0,0 +1,14 @@
+// using Microsoft.EntityFrameworkCore;
+// using Microsoft.EntityFrameworkCore.Metadata.Builders;
+// using Prime.Models;
+
+// namespace Prime.Configuration
+// {
+//     public class SiteConfiguration : IEntityTypeConfiguration<Site>
+//     {
+//         public void Configure(EntityTypeBuilder<Site> builder)
+//         {
+//             builder.HasDiscriminator();
+//         }
+//     }
+// }

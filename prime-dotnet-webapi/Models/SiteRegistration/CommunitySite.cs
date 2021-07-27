19a60256dd5e638e68273bd1c83f8e8b90bbdacc
James <jameshollinger.ee@gmail.com>
2021-07-27 12:32:54 -0700

    WIP

--- prime-dotnet-webapi/Models/SiteRegistration/CommunitySite.cs
+++ prime-dotnet-webapi/Models/SiteRegistration/CommunitySite.cs
@@ -10,23 +10,10 @@ namespace Prime.Models
 {
     public class CommunitySite : Site
     {
-        public CommunitySite()
-        {
-            // Initialize collections to prevent null exception on computed properties
-            // like `Status`
-            SiteStatuses = new List<SiteStatus>();
-        }
-
-
-        [Key]
-        public int Id { get; set; }
-
         public int OrganizationId { get; set; }
 
         [JsonIgnore]
         public Organization Organization { get; set; }
-
-        public PhysicalAddress PhysicalAddress { get; set; }
 
         public int? AdministratorPharmaNetId { get; set; }
 
@@ -49,75 +36,8 @@ namespace Prime.Models
         [JsonIgnore]
         public CareSetting CareSetting { get; set; }
 
-        public string PEC { get; set; }
-
-        public string DoingBusinessAs { get; set; }
-
-        public bool Completed { get; set; }
-
-        public DateTimeOffset? SubmittedDate { get; set; }
-
-        public ICollection<SiteStatus> SiteStatuses { get; set; }
-
-        public DateTimeOffset? ApprovedDate { get; set; }
-
         public ICollection<SiteVendor> SiteVendors { get; set; }
 
         public BusinessLicence BusinessLicence { get; set; }
-
-        public ICollection<RemoteUser> RemoteUsers { get; set; }
-
-        public int? AdjudicatorId { get; set; }
-
-        public Admin Adjudicator { get; set; }
-
-        [JsonIgnore]
-        public ICollection<SiteRegistrationReviewDocument> SiteRegistrationReviewDocuments { get; set; }
-
-        [JsonIgnore]
-        public ICollection<SiteAdjudicationDocument> SiteAdjudicationDocuments { get; set; }
-
-        public ICollection<SiteRegistrationNote> SiteRegistrationNotes { get; set; }
-
-        public ICollection<BusinessDay> BusinessHours { get; set; }
-        /// <summary>
-        /// Days in which the business has any business hours.
-        /// Only the time portion of the input parameter is considered.
-        /// </summary>
-        public IEnumerable<DayOfWeek> DaysOpen(DateTimeOffset? atTime = null)
-        {
-            return BusinessHours
-                .Where(h => atTime == null || h.IsOpen(atTime.Value))
-                .Select(b => b.Day)
-                .Distinct();
-        }
-
-        public SiteStatus AddStatus(SiteStatusType siteStatusType)
-        {
-            var newStatus = SiteStatus.FromType(siteStatusType, Id);
-
-            if (SiteStatuses == null)
-            {
-                SiteStatuses = new List<SiteStatus>();
-            }
-            SiteStatuses.Add(newStatus);
-
-            return newStatus;
-        }
-
-        /// <summary>
-        /// Gets the most recent Status of the Site.
-        /// </summary>
-        [NotMapped]
-        [Computed]
-        public SiteStatusType Status
-        {
-            get => (SiteStatuses.Count > 0 ?
-                SiteStatuses
-                .OrderByDescending(s => s.StatusDate)
-                .ThenByDescending(s => s.Id)
-                .FirstOrDefault().StatusType :
-                SiteStatusType.Active);
-        }
     }
 }
--- prime-dotnet-webapi/Models/SiteRegistration/Site.cs
+++ prime-dotnet-webapi/Models/SiteRegistration/Site.cs
@@ -11,6 +11,84 @@ namespace Prime.Models
     [Table("Site")]
     public abstract class Site : BaseAuditable
     {
+        protected Site()
+        {
+            // Initialize collections to prevent null exception on computed properties
+            // like `Status`
+            SiteStatuses = new List<SiteStatus>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        public PhysicalAddress PhysicalAddress { get; set; }
+
+        public DateTimeOffset? SubmittedDate { get; set; }
+
+        public ICollection<SiteStatus> SiteStatuses { get; set; }
+
+        public DateTimeOffset? ApprovedDate { get; set; }
+
+        public ICollection<RemoteUser> RemoteUsers { get; set; }
+
+        public string PEC { get; set; }
+
+        public string DoingBusinessAs { get; set; }
+
+        public bool Completed { get; set; }
+
+        public int? AdjudicatorId { get; set; }
+
+        public Admin Adjudicator { get; set; }
+
+        [JsonIgnore]
+        public ICollection<SiteRegistrationReviewDocument> SiteRegistrationReviewDocuments { get; set; }
+
+        [JsonIgnore]
+        public ICollection<SiteAdjudicationDocument> SiteAdjudicationDocuments { get; set; }
+
+        public ICollection<SiteRegistrationNote> SiteRegistrationNotes { get; set; }
+
+        public ICollection<BusinessDay> BusinessHours { get; set; }
+        /// <summary>
+        /// Days in which the business has any business hours.
+        /// Only the time portion of the input parameter is considered.
+        /// </summary>
+        public IEnumerable<DayOfWeek> DaysOpen(DateTimeOffset? atTime = null)
+        {
+            return BusinessHours
+                .Where(h => atTime == null || h.IsOpen(atTime.Value))
+                .Select(b => b.Day)
+                .Distinct();
+        }
+
+        public SiteStatus AddStatus(SiteStatusType siteStatusType)
+        {
+            var newStatus = SiteStatus.FromType(siteStatusType, Id);
+
+            if (SiteStatuses == null)
+            {
+                SiteStatuses = new List<SiteStatus>();
+            }
+            SiteStatuses.Add(newStatus);
+
+            return newStatus;
+        }
+
+        /// <summary>
+        /// Gets the most recent Status of the Site.
+        /// </summary>
+        [NotMapped]
+        [Computed]
+        public SiteStatusType Status
+        {
+            get => (SiteStatuses.Count > 0 ?
+                SiteStatuses
+                .OrderByDescending(s => s.StatusDate)
+                .ThenByDescending(s => s.Id)
+                .FirstOrDefault().StatusType :
+                SiteStatusType.Active);
+        }
 
     }
 }
